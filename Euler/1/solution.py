380b35881320ec10d0f14245fce08c5deafd14b1
Ayjrin <adrian.m.tull@gmail.com>
2025-08-26 09:58:04 -0700

    Euler 2 fix and 3 start

--- Euler/1/solution.py
+++ Euler/1/solution.py
@@ -3,12 +3,11 @@
 # Find the sum of all the multiples of $3$ or $5$ below $1000$.
 
 def solution():
-    is_multiple = []
-
+    answer = 0
     for i in range(1000):
         if i % 3 == 0 or i % 5 == 0:
-            is_multiple.append(i)
+            answer += i
 
-    print(is_multiple)
+    print(answer)
 
 solution()
